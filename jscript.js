7ef4ae0ac05350eddac4eace8b284d554797445f
Daniela Mayer <daniela-mayer@live.com>
2023-05-22 21:53:56 -0300

    agregado de js

---
+++ jscript.js
@@ -0,0 +1,12 @@
+const bienvenidos = alert ("Bienvenid@ groover@");
+
+let nombreVisitante = prompt ("¿Contame, cuál es tu nombre?");
+let saludoAlVisitante = alert ("Mucho gusto " + nombreVisitante);
+
+let hablemos = prompt ( nombreVisitante + " ¿Te gustaría saber más sobre música electrónica? Pon aceptar si estás de acuerdo :)");
+
+if (hablemos = "si"){
+    console.log ("Entonces, te invito a navegar por esta web");
+}else{
+    console.warn ("Pués cuando gustes, puedes volver");
+}
